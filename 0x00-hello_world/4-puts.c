198ad3066eca50db3c286154124bf7db86b2e305
Brandon Gowera <gowerabrandon1@gmail.com>
2022-09-08 07:38:07 +0200

    mandatory tasks and readme

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - Entry Point->prints the first half of a sentence
+ *Return: 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
