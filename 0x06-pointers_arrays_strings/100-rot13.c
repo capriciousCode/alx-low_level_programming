40119af3cefe17f41511f9042a52ec8c4a2022db
Brandon Gowera <gowerabrandon1@gmail.com>
2022-09-22 04:26:50 +0200

    101 and 102

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ *
+ * @c: characters to be encoded
+ *
+ * Return: encoded string
+ */
+
+char *rot13(char *c)
+{
+	int alpha[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+		     'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		     'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+		     'W', 'X', 'Y', 'Z'};
+	int rot[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		     'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+		     'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+		     'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+		     'J', 'K', 'L', 'M'};
+	int i, j;
+
+	for (j = 0; c[j] != '\0'; j++)
+	{
+		for (i = 0; alpha[i] != '\0' && c[j] != alpha[i]; i++)
+			;
+		if (c[j] == alpha[i])
+			c[j] = rot[i];
+	}
+
+	return (c);
+}
